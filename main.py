8655767a4b2ff22bf42de5661c763e01afa6865d
Lim Meng Shin <62892957+LimMengShin@users.noreply.github.com>
2022-01-08 18:10:59 +0800

    Add questions and their weightages

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Disclaimer: This test cannot be used to substitute professional medical advice nor consulatation. Please seek medical help if you feel unwell.")
+print("Disclaimer: This test cannot be used to substitute professional medical advice nor consulatation. Please seek medical help if you feel unwell.\n")
+
+questions = [
+    "Have you been bitten by a mosquito in the last 4 - 10 days?",
+    "Did you experience high fever (above 40ºC) in the last 4 - 10 days?",
+    "Have you experienced headaches in the last 4 - 10 days?",
+    "Have you experienced muscle, bone or joint pains in the last 4 - 10 days",
+    "Have you experienced nausea or vomiting in the last 4 - 10 days?",
+    "Have you experienced swollen glands in the last 4 - 10 days?",
+    "Have you experienced rashes in the last 4 - 10 days?",
+    "Have you experienced severe stomach pain in the last 4 - 10 days?",
+    "Have you experienced bleeding in your gums and nose in the last 4 - 10 days?",
+    "Have you observed blood in your urine, stools or vomit in the last 4 - 10 days?",
+    "Have you observed any bruises on your skin in the last 4 - 10 days?",
+    "Have you experienced difficult or rapid breathing in the last 4 - 10 days?",
+    "Have you experienced fatigue in the last 4 - 10 days?",
+    "Have you felt more irritable or restless in the last 4 - 10 days?",
+]
+weightages = [20, 15, 10, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 5]
