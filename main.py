ee5d82b57eadb6ad7ba3d6534c1c4d75a29a241a
Ysabelle <74769284+ysabelle2006@users.noreply.github.com>
2022-01-19 12:04:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import geocoder
+import geocoder #Import all modules used
 import requests
 import json
 from haversine import haversine
 
-print("Disclaimer: This test cannot be used to substitute professional medical advice nor consulatation. Please seek medical help if you feel unwell.\n")
+print("Disclaimer: This test cannot be used to substitute professional medical advice nor consulatation. Please seek medical help if you feel unwell.\n") #disclaimer
 
 questions = [
     "Have you been bitten by a mosquito in the last 4 - 10 days?",
@@ -20,58 +20,58 @@ questions = [
     "Have you experienced difficult or rapid breathing in the last 4 - 10 days?",
     "Have you experienced fatigue in the last 4 - 10 days?",
     "Have you felt more irritable or restless in the last 4 - 10 days?",
-]
-weightages = [20, 15, 10, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 5]
-answers = []
+] #Questions to ask user
+weightages = [20, 15, 10, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 5] #Weightages of each question
+answers = [] #Empty list to record answers of users
 
 for i in range(len(questions)):
     print(f"{i + 1}. {questions[i]}")
 
-    while True:
-        answer = input("Enter answer [y/n]: ")
+    while True: #Forever loop for input validation
+        answer = input("Enter answer [y/n]: ") #users input answer 
         print()
-        if answer.lower() == "y" or answer.lower() == "n":
+        if answer.lower() == "y" or answer.lower() == "n": #input validation check
             answers.append(answer)
-            break
+            break #Break after input is validated
         else:
-            print("Your answer is invalid.\nRe-", end="")
+            print("Your answer is invalid.\nRe-", end="") #Error message
 
-total = 0
+total = 0 #initialising total points of user based on their answers and their question weightages 
 
 for i in range(len(answers)):
     if answers[i] == "y":
-        total += weightages[i]
+        total += weightages[i] #adding of weightages to total if they answered yes
 
 try:
-    g = geocoder.ip("me")
+    g = geocoder.ip("me") #finding latitude and longitude of user from module --> geocoder
     lat_lng = g.latlng
 
-    nea_api_url = "https://www.nea.gov.sg/api/OneMap/GetMapData/DENGUE_CLUSTER"
+    nea_api_url = "https://www.nea.gov.sg/api/OneMap/GetMapData/DENGUE_CLUSTER" #use of API to find location of dengue clusters in SG
     response = requests.get(nea_api_url)
 
-    dengue_dict = {}
+    dengue_dict = {} #initialising of dictionary to store data from API
     results = json.loads(response.json().replace("\\", "")).get("SrchResults")[1:]
-    for i in range(len(results)):
+    for i in range(len(results)): #storing data from API into dictionary
         cluster = results[i]
         dengue_dict.update(
             {
                 i: {
-                    "location": cluster.get("DESCRIPTION"),
-                    "case_size": int(cluster.get("CASE_SIZE")),
-                    "lat_lng": [[float(i) for i in x] for x in [i.split(",") for i in cluster.get("LatLng").split("|")]]
+                    "location": cluster.get("DESCRIPTION"), #stores location of cluster (ie streets)
+                    "case_size": int(cluster.get("CASE_SIZE")), #stores number of cases for that cluster
+                    "lat_lng": [[float(i) for i in x] for x in [i.split(",") for i in cluster.get("LatLng").split("|")]] #stores latitude and longitude of cluster
                 }
             }
         )
 
-    clusters_list = []
+    clusters_list = [] #initialise empty list to 
     for cluster in dengue_dict.values():
         for i in cluster.get("lat_lng"):
-            if haversine(lat_lng, i) < 0.2:
-                clusters_list.append([cluster.get("location"), cluster.get("case_size")])
+            if haversine(lat_lng, i) < 0.2: #if distance between latitude and longitude of cluster and user is < 200m (uses haversine formula)
+                clusters_list.append([cluster.get("location"), cluster.get("case_size")]) #record location and case size into empty list
                 break
 
-    clusters_list_length = len(clusters_list)
-    if clusters_list_length == 0:
+    clusters_list_length = len(clusters_list) #check number of clusters that is within 200m radius of user
+    if clusters_list_length == 0: #Output 
         print("You are not near any dengue clusters.")
     elif clusters_list_length == 1:
         print(f"You are near 1 dengue cluster:\n{clusters_list[0][0]} | {clusters_list[0][1]} dengue cases")
