da71e6f88eba7dcbe012e195ef35bfc171edbb93
LimMengShin <lim_meng_shin@s2019.ssts.edu.sg>
2022-01-19 10:30:47 +0800

    Add functionality to list dengue clusters near user

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import geocoder
 import requests
 import json
+from haversine import haversine
 
 print("Disclaimer: This test cannot be used to substitute professional medical advice nor consulatation. Please seek medical help if you feel unwell.\n")
 
@@ -43,8 +44,7 @@ for i in range(len(answers)):
 
 try:
     g = geocoder.ip("me")
-    lat = g.latlng[0]
-    long = g.latlng[1]
+    lat_lng = g.latlng
 
     nea_api_url = "https://www.nea.gov.sg/api/OneMap/GetMapData/DENGUE_CLUSTER"
     response = requests.get(nea_api_url)
@@ -58,16 +58,32 @@ try:
                 i: {
                     "location": cluster.get("DESCRIPTION"),
                     "case_size": int(cluster.get("CASE_SIZE")),
-                    "alert_level": cluster.get("SYMBOLCOLOR"),
                     "lat_lng": [[float(i) for i in x] for x in [i.split(",") for i in cluster.get("LatLng").split("|")]]
                 }
             }
         )
+
+    clusters_list = []
+    for cluster in dengue_dict.values():
+        for i in cluster.get("lat_lng"):
+            if haversine(lat_lng, i) < 0.2:
+                clusters_list.append([cluster.get("location"), cluster.get("case_size")])
+                break
+
+    clusters_list_length = len(clusters_list)
+    if len(clusters_list) == 0:
+        print("You are not near any dengue clusters.")
+    elif len(clusters_list) == 1:
+        print(f"You are near 1 dengue cluster:\n{clusters_list[0][0]} | {clusters_list[0][1]} dengue cases")
+    else:
+        print(f"You are near {clusters_list_length} dengue clusters:")
+        for i in range(clusters_list_length):
+            print(f"{i+1}. {clusters_list[i][0]} | {clusters_list[i][1]} dengue cases")
 except TypeError:
     print("Unable to check for dengue clusters near you. Check your internet connection and try again.")
 
 percentage = total / 120 * 100
-print("Chance of having dengue: {:0.2f}%".format(percentage))
+print("\nChance of having dengue: {:0.2f}%".format(percentage))
 if percentage >= 80:
     print("Very high chance of having dengue. Please visit a doctor now.")
 elif percentage >= 50:
