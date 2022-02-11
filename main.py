babd18524f0ae7a37c78301c8d74a916c29654be
LimMengShin <lim_meng_shin@s2019.ssts.edu.sg>
2022-02-11 15:11:40 +0800

    Refactor comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Imports
+# Imports
 from flask import Flask, render_template, request
 import geocoder
 import requests
@@ -8,66 +8,81 @@ from haversine import haversine
 from questions import *
 
 
+# Constants
 NEA_API_URL = "https://www.nea.gov.sg/api/OneMap/GetMapData/DENGUE_CLUSTER"
 
-app = Flask(__name__) # Initialise flask
+
+# Initialise Flask
+app = Flask(__name__)
 
 
+# Calculate chance of user having dengue based on user's input in percentage
 def calculate_percentage():
-    total = 0  # Initialising total points of user based on their answers and their question weightages
+    total = 0  # Initialise total points of user based on their answers and the question weightages
 
     for question, weightage in questions.items():
-        answer = request.form[question]  # users input answer
+        answer = request.form[question]  # User's input
         if answer == "Yes":
-            total += weightage  # adding of weightages to total if they answered yes
+            total += weightage  # Add weightages to total if user answered "Yes"
 
-    percentage = total / sum(questions.values()) * 100  # Calculating percentage likelihood of dengue
+    percentage = total / sum(questions.values()) * 100  # Calculate user's chance of having dengue in percentage
     return percentage
 
 
+# Get location (latitude, longitude) of user using the Geocoder module
 def get_location():
-    g = geocoder.ip("me")  # finding latitude and longitude of user from module --> geocoder
+    g = geocoder.ip("me")
     lat_lng = g.latlng
     return lat_lng
 
 
+# Make a "GET" request to the NEA API
 def get_data(url):
     response = requests.get(url)
     return response
 
 
+# Format response and store formatted response in a dictionary
 def format_data(response):
-    dengue_dict = {} #initialising of dictionary to store data from API
-    results = json.loads(response.json().replace("\\", "")).get("SrchResults")[1:]  # Clean data [Chooses which data to store]
-    for i in range(len(results)):  # storing data from API into dictionary
+    dengue_dict = {}  # Initialise empty dictionary to store formatted response
+    results = json.loads(response.json().replace("\\", "")).get("SrchResults")[1:]  # Remove backslashes from response
+
+    # Store formatted response in the dictionary "dengue_dict"
+    for i in range(len(results)):
         cluster_raw = results[i]
         dengue_dict.update(
             {
                 i: {
-                    "location": cluster_raw.get("DESCRIPTION"),  # stores location of cluster (ie streets)
-                    "case_size": int(cluster_raw.get("CASE_SIZE")),  # stores number of cases for that cluster
-                    "lat_lng": [[float(i) for i in x] for x in [i.split(",") for i in cluster_raw.get("LatLng").split("|")]]
-                    # stores latitude and longitude of cluster
+                    "location": cluster_raw.get("DESCRIPTION"),  # Store location of cluster (i.e. street name)
+                    "case_size": int(cluster_raw.get("CASE_SIZE")),  # Store number of cases for that cluster
+                    "lat_lng": [[float(i) for i in x] for x in [i.split(",") for i in cluster_raw.get("LatLng").split("|")]]  # Store location (latitude and longitude) of cluster
                 }
             }
         )
     return dengue_dict
 
 
+# Check for nearby clusters (less than 200 metres from user) and store them in a list
 def check_nearby_clusters(dengue_dict, lat_lng):
-    clusters_list = []  # initialise empty list to store cluster info
+    clusters_list = []  # Initialise empty list to store cluster info
+
+    # Loop through each cluster in the "dengue_dict" dictionary
     for cluster in dengue_dict.values():
         for i in cluster.get("lat_lng"):
-            if haversine(lat_lng, i) < 0.2:  # if distance between cluster and user is < 200m (uses haversine formula)
-                clusters_list.append([cluster.get("location"), cluster.get("case_size")])  # record location and case size into empty list
+            if haversine(lat_lng, i) < 0.2:  # Using the haversine module, check if distance between cluster location and user's location is less than 200 metres
+                clusters_list.append([cluster.get("location"), cluster.get("case_size")])  # Append location and case size of cluster to "clusters_list" list
                 break
     return clusters_list
 
 
+# Add HTML of dengue cluster(s) nearby, if any, to "clusters_html" string,
+# and increase percentage depending on number of nearby clusters
 def location_results(clusters_list, clusters_html, percentage):
-    clusters_list_length = len(clusters_list)  # check number of clusters that is within 200m radius of user
+    clusters_list_length = len(clusters_list)  # Count number of nearby clusters
+
+    # Add HTML of dengue cluster(s) nearby, if any, to "clusters_html" string
     if clusters_list_length == 0:
-        clusters_html += "<h3 class='center' >No dengue clusters nearby.</h3>"  # Varies html msg based on nearby clusters
+        clusters_html += "<h3 class='center' >No dengue clusters nearby.</h3>"
     else:
         clusters_html += f"<h3>{clusters_list_length} dengue cluster(s) nearby:</h3>"
         clusters_html += "<ol>"
@@ -75,20 +90,24 @@ def location_results(clusters_list, clusters_html, percentage):
             clusters_html += f"<li>{clusters_list[i][0]} | {clusters_list[i][1]} dengue cases</li>"
         clusters_html += "</ol>"
 
-        if clusters_list_length == 1:  # Adds percentage likelihood of having dengue based on no. of nearby cluster
+        # Increase percentage depending on number of nearby clusters
+        if clusters_list_length == 1:
             percentage += 3
         else:
             percentage += 5
 
-        if percentage > 100:  # ensures that % will not exceed 100% due to nearby clusters
+        # Ensure that percentage will not be more than 100%
+        if percentage > 100:
             percentage = 100
     return clusters_html, percentage
 
 
+# Add HTML of chance of having dengue percentage and advice on what to do, if applicable, to "final" string
 def final_results(final, percentage):
-    final += "<h2 class='center' >Chance of having dengue: <u>{:0.2f}%</u></h2>".format(
-        percentage)  # Outputs percentage likelihood
-    if percentage >= 80:  # Outputs advice based on percentage
+    final += "<h2 class='center' >Chance of having dengue: <u>{:0.2f}%</u></h2>".format(percentage)  # Output percentage
+
+    # Output advice based on percentage
+    if percentage >= 80:
         final += "<h2 class='center' >Very high chance of having dengue. Please visit a doctor now.</h2>"
     elif percentage >= 50:
         final += "<h2 class='center' >High chance of having dengue. Please visit a doctor soon.</h2>"
@@ -99,25 +118,26 @@ def final_results(final, percentage):
     return final
 
 
-#Show website design from main.html for main page & shows questions
+# Render page template from main.html
 @app.route("/")
 def main_page():
     return render_template("main.html", q=questions.keys())
 
 
-#Show website design from help.html for help page
+# Render page template from help.html
 @app.route("/help")
 def help_page():
     return render_template("help.html")
 
 
-@app.route("/results", methods=["POST"]) #Retrieves data submitted by user on main page
+# Display results page and returns results HTML
+@app.route("/results", methods=["POST"])
 def results_page():
-    final = "" #Initialise html which will be shown on results page
+    final = ""  # Initialise empty string for HTML which will be shown on results page
 
     percentage = calculate_percentage()
 
-    clusters_html = "" #Initialise html to show user if nearby dengue clusters present
+    clusters_html = ""  # Initialise empty string for HTML to show user nearby dengue clusters
 
     try:
         lat_lng = get_location()
@@ -130,15 +150,15 @@ def results_page():
 
         clusters_html, percentage = location_results(clusters_list, clusters_html, percentage)
 
-    except requests.ConnectionError: #If no connection, changes cluster msg
+    except requests.ConnectionError:  # Add message to "clusters_html" string when internet connection is unavailable
         clusters_html += "<h3 class='center' >Unable to check for dengue clusters near you. Check your internet connection and try again.</h3>"
 
-    except TypeError:
+    except TypeError:  # Add message to "clusters_html" string when there is an error
         clusters_html += "<h3 class='center' >Unable to check for dengue clusters near you. There may be an error with your internet connection.<br>Try using a different internet connection.</h3>"
 
     final = final_results(final, percentage)
 
-    #Displays results on results page
+    # Top portion of HTML of results page
     html_top = """
     <!DOCTYPE html>
     <html lang='en'>
@@ -176,11 +196,15 @@ def results_page():
     </head>
     <body>
     """
+
+    # Bottom portion of HTML of results page
     html_bottom = "<button onclick='history.back()'>Go Back</button></body></html>"
 
+    # Return results HTML on results page
     final = html_top + final + "<hr>" + clusters_html + html_bottom
     return final
 
 
-if __name__ == "__main__": #Run the programme
+# Run the programme
+if __name__ == "__main__":
     app.run()
--- questions.py
+++ questions.py
@@ -1,3 +1,4 @@
+# Questions (with weightages) to ask user
 questions = {
     "Have you been bitten by a mosquito in the last 4 - 10 days?": 20,
     "Have you experienced high fever (above 40ºC) in the last 4 - 10 days?": 15,
@@ -13,4 +14,4 @@ questions = {
     "Have you experienced difficult or rapid breathing in the last 4 - 10 days?": 5,
     "Have you experienced fatigue in the last 4 - 10 days?": 5,
     "Have you felt more irritable or restless in the last 4 - 10 days?": 5,
-} #Questions (with weightages) to ask user
+}
